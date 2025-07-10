48a532585fd4b97295aaebaf0348345cd4848894
omovigho <100778865+omovigho@users.noreply.github.com>
2025-07-10 12:06:02 +0100

    group member

---
+++ app/models/group_member.py
@@ -0,0 +1,16 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy import Column
+from datetime import datetime
+
+class GroupMember(SQLModel, table=True):
+    __tablename__ = "group_members"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    group_session_id: int = Field(foreign_key="group_sessions.id")
+    group_name: str  # e.g. "Team A", "Group 1"
+    attributes: dict = Field(sa_column=Column(JSONB))
+    #attributes: dict = Field(sa_column_kwargs={"type_": JSONB})  # dynamic member info
+    member_identifier: str  # unique ID within the session
+    joined_at: datetime = Field(default_factory=datetime.now)
