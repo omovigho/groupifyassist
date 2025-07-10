83cac0d4769b980986c63607637cddbc36b5cc5d
omovigho <100778865+omovigho@users.noreply.github.com>
2025-07-10 12:15:14 +0100

    database configuration

---
+++ app/core/database.py
@@ -0,0 +1,35 @@
+import sys
+from pathlib import Path
+from sqlmodel import SQLModel
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlmodel.ext.asyncio.session import AsyncSession
+import os
+from dotenv import load_dotenv
+from typing import Annotated, AsyncGenerator
+from fastapi import Depends
+
+# Add root directory to sys.path
+sys.path.append(str(Path(__file__).resolve().parents[2]))
+
+# Now safely import models
+import app.models  # ✅ This will now work
+
+# Load env
+current_dir = Path(__file__).parent
+config_path = current_dir / "config.env"
+load_dotenv(dotenv_path=config_path)
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
+
+async def init_db():
+    print(f"Connecting to database at {DATABASE_URL}")
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
