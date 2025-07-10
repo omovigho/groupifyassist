379c783b4dbe0f2be37efa6073cfbe66ebd0e19e
omovigho <100778865+omovigho@users.noreply.github.com>
2025-07-10 12:13:13 +0100

    selection session model

---
+++ app/models/selection_session.py
@@ -0,0 +1,15 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+from datetime import datetime
+
+class SelectionSession(SQLModel, table=True):
+    __tablename__ = "selection_sessions"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    description: str
+    code: str = Field(index=True, unique=True)
+    host_id: int = Field(foreign_key="users.id")
+    description: Optional[str] = None
+    created_at: datetime = Field(default_factory=datetime.now)
+    expires_at: Optional[datetime] = None
