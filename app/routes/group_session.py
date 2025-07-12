81d1f101c27ee706c77fab34fdcad498121c6e7c
omovigho <100778865+omovigho@users.noreply.github.com>
2025-07-12 21:58:21 +0100

    routes for grouping feature

---
+++ app/routes/group_session.py
@@ -0,0 +1,24 @@
+# app/routes/group_session.py
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlmodel.ext.asyncio.session import AsyncSession
+from app.schemas.group_session import GroupSessionCreate, GroupSessionRead
+from app.services.group_session_service import create_group_session
+from app.core.dependencies import get_current_user
+from app.core.database import get_session, SessionDep
+from app.models.user import User
+
+
+router = APIRouter(prefix="/groups", tags=["Grouping"])
+
+
+@router.post("/create", response_model=GroupSessionRead)
+async def create_group(
+    data: GroupSessionCreate,
+    session: SessionDep,
+    current_user: User = Depends(get_current_user)
+):
+    try:
+        group = await create_group_session(data, current_user.id, session)
+        return group
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
