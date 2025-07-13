2bd7431c41056a3ece06f17ec12fa8a0f2dd889f
omovigho <100778865+omovigho@users.noreply.github.com>
2025-07-13 21:14:00 +0100

    removing verified, verification code, and expire fields

--- app/models/user.py
+++ app/models/user.py
@@ -9,14 +9,11 @@ class User(SQLModel, table=True):
     __table_args__ = {"extend_existing": True}
     
     id: Optional[int] = Field(default=None, primary_key=True)
-    email: str = Field(index=True, unique=True, nullable=False)
+    email: str #= Field(index=True, unique=True, nullable=False)
     password: str
     country: str
     is_active: bool = Field(default=False)
     created_at: datetime = Field(default_factory=datetime.now)
-    verified: bool = Field(default=False)
-    verification_code: Optional[str] = None
-    verification_expires_at: Optional[datetime] = None
 
     class Config:
         arbitrary_types_allowed = True
