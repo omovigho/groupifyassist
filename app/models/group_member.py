a061ccb0dc3b8a7bb8b3e64480951421074a43ea
omovigho <100778865+omovigho@users.noreply.github.com>
2025-07-13 21:06:22 +0100

    adding group id and member data

--- app/models/group_member.py
+++ app/models/group_member.py
@@ -9,9 +9,9 @@ class GroupMember(SQLModel, table=True):
     __table_args__ = {"extend_existing": True}
 
     id: Optional[int] = Field(default=None, primary_key=True)
-    group_session_id: int = Field(foreign_key="group_sessions.id")
+    group_id: int = Field(foreign_key="groups.id")
     group_name: str  # e.g. "Team A", "Group 1"
-    attributes: dict = Field(sa_column=Column(JSONB))
+    member_data: dict = Field(sa_column=Column(JSONB))
     #attributes: dict = Field(sa_column_kwargs={"type_": JSONB})  # dynamic member info
     member_identifier: str  # unique ID within the session
     joined_at: datetime = Field(default_factory=datetime.now)
