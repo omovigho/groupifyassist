c1e84d3ab216a23d9e49d0feb37c10c492e93930
omovigho <100778865+omovigho@users.noreply.github.com>
2025-07-17 05:44:37 +0100

    routes for adding join group and field

--- app/routes/group_session.py
+++ app/routes/group_session.py
@@ -6,6 +6,9 @@ from app.services.group_session_service import create_group_session
 from app.core.dependencies import get_current_user
 from app.core.database import get_session, SessionDep
 from app.models.user import User
+from app.services.group_session_service import validate_code_and_get_fields, join_group
+from app.schemas.group_session import GroupJoinRequest, GroupJoinResponse
+from app.schemas.group_session import MessageResponse
 
 
 router = APIRouter(prefix="/api/groups", tags=["Grouping"])
@@ -22,3 +25,24 @@ async def create_group(
         return group
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+
+@router.get("/fields", response_model=dict)
+async def get_fields_for_code(code: str, session: SessionDep):
+    try:
+        return await validate_code_and_get_fields(code, session)
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+
+@router.post("/join", response_model=GroupJoinResponse)
+async def join_group_with_code(payload: GroupJoinRequest, session: SessionDep):
+    try:
+        return await join_group(
+            code=payload.code,
+            member_identifier=payload.member_identifier,
+            member_data=payload.member_data,
+            session=session
+        )
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
