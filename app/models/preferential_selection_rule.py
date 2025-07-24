0855f3b11bb78e0ccc9a14f3bd5691cf7aa2967b
omovigho <100778865+omovigho@users.noreply.github.com>
2025-07-24 19:56:32 +0100

    preferential rule for selection

---
+++ app/models/preferential_selection_rule.py
@@ -0,0 +1,12 @@
+# app/models/preferential_grouping_rule.py
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+class PreferentialSelectionRule(SQLModel, table=True):
+    __tablename__ = "preferential_selection_rules"
+    __table_args__ = {"extend_existing": True}
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    selection_session_id: int = Field(foreign_key="selection_sessions.id")
+    field_key: str  # e.g. "gender"
+    specific_number: int
