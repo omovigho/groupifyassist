641a51fc20091040138d037032f80d923d644620
omovigho <100778865+omovigho@users.noreply.github.com>
2025-07-31 23:20:09 +0100

    adding selection routes

--- app/routes/selection_session.py
+++ app/routes/selection_session.py
@@ -1,21 +1,18 @@
 # app/routes/group_session.py
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel.ext.asyncio.session import AsyncSession
-from app.schemas.group_session import GroupSessionCreate, GroupSessionRead
+from app.schemas.selection_session import SelectionSessionCreate, SelectionSessionRead
 from app.services.group_session_service import create_group_session
 from app.core.dependencies import get_current_user
 from app.core.database import get_session, SessionDep
 from app.models.user import User
-from app.services.group_session_service import validate_code_and_get_fields, join_group
-from app.schemas.group_session import GroupJoinRequest, GroupJoinResponse
-from app.schemas.group_session import MessageResponse
 
 
 router = APIRouter(prefix="/api/selections", tags=["Selection"])
 
-@router.post("/create", response_model=GroupSessionRead)
+@router.post("/create", response_model=SelectionSessionRead)
 async def create_group(
-    data: GroupSessionCreate,
+    data: SelectionSessionCreate,
     session: SessionDep,
     current_user: User = Depends(get_current_user)
 ):
