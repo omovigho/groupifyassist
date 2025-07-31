b762592220e7a46e867139871d15d4dbf8fdc2fc
omovigho <100778865+omovigho@users.noreply.github.com>
2025-07-31 23:18:55 +0100

    adding identifier

--- app/models/selection_session.py
+++ app/models/selection_session.py
@@ -7,7 +7,8 @@ class SelectionSession(SQLModel, table=True):
     __table_args__ = {"extend_existing": True}
     
     id: Optional[int] = Field(default=None, primary_key=True)
-    title: str
+    name: str
     description: Optional[str] = None
     code_id: int = Field(foreign_key="access_codes.id")
+    member_identifier: str  # unique ID within the session
     host_id: int = Field(foreign_key="users.id")
