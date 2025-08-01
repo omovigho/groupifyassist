422854ae92830282f7835a0317f5b38ce100e854
omovigho <100778865+omovigho@users.noreply.github.com>
2025-08-01 04:35:06 +0100

    changing from group session to selection session

--- app/routes/selection_session.py
+++ app/routes/selection_session.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel.ext.asyncio.session import AsyncSession
 from app.schemas.selection_session import SelectionSessionCreate, SelectionSessionRead
-from app.services.group_session_service import create_group_session
+from app.services.selection_service import create_selection_session
 from app.core.dependencies import get_current_user
 from app.core.database import get_session, SessionDep
 from app.models.user import User
@@ -11,13 +11,13 @@ from app.models.user import User
 router = APIRouter(prefix="/api/selections", tags=["Selection"])
 
 @router.post("/create", response_model=SelectionSessionRead)
-async def create_group(
+async def create_selection(
     data: SelectionSessionCreate,
     session: SessionDep,
     current_user: User = Depends(get_current_user)
 ):
     try:
-        group = await create_group_session(data, current_user.id, session)
-        return group
+        selection = await create_selection_session(data, current_user.id, session)
+        return selection
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
